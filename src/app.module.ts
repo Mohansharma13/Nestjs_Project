c50aef71be7418b8e4767cb665d7bb2a9833e62b
Mohan Sharma <msharma13613@gmail.com>
2025-04-05 20:08:40 +0530

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { DatabasenestService } from './g/databasenest/databasenest.service';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabasenestService],
 })
 export class AppModule {}
